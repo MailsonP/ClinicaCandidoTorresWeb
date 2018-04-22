1b6b3701ef9240a70781bd295ef8976353bb0752
Jonathan Costa Matos <37823427+JonathanMatos97@users.noreply.github.com>
2018-04-22 15:31:36 -0300

    Organizao da estrutura e Criação da Classe Usuario

---
+++ ClinicaCandidoTorresWebProject/Usuario/Usuario.php
@@ -0,0 +1,45 @@
+<?php
+    
+//autor - Jonathan
+class Usuario {
+    
+    //Criando os atributos
+    private $nome;
+    private $login;
+    private $senha;
+    private $tipoUsuario;
+    
+    //Gerando os Geets e Seets
+    function setNome($nome){
+        $this->nome = $nome;
+    }
+    
+     function setLogin($login){
+        $this->login = $login;
+    }
+    
+     function setSenha($senha){
+        $this->senha = $senha;
+    }
+    
+     function setTipoUsuario($tipoUsuario){
+        $this -> tipoUsuario = $tipoUsuario;
+    }
+    
+    function getNome(){
+        return $this->nome;
+    }
+    
+    function getLogin(){
+        return $this->login;
+    }
+    
+    function getSenha(){
+        return $this->senha;
+    }
+    
+    function getTipoUsuario(){
+        return $this->tipoUsuario;
+    }
+    
+}
