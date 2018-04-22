4da24b75d0404c53910f2fb31460a7dbd7e83367
Jonathan Costa Matos <37823427+JonathanMatos97@users.noreply.github.com>
2018-04-22 15:41:32 -0300

    Criação pre-definida da interface de Cadastro de Usuario

---
+++ ClinicaCandidoTorresWebProject/Cadastro de Usuario.php
@@ -0,0 +1,32 @@
+<html lang="pt-br">
+    <head>
+        <title>Cadastro Usuario</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="Cadastro" style="text-align:center">
+            
+        <fieldset>
+            <legend>Cadastro de Usuario</legend>   
+            <form action="#" method="POST">
+            
+                <p> Nome: </p>
+                <input type="text" name="nome">
+                <p> Login</p>
+                <input type="text" name="login" >
+                <p> Senha</p>
+                <input type="password" name="senha" >
+                <p> Tipo de Usuario: </p>
+                <select name="tipoUsuario">
+                <option value="adm"> Administrador </option>
+                <option value="user"> Usuario </option>        
+                </select>
+                <br>
+                <br>
+                <button type="button">Cadastrar</button> 
+            </form>
+        </fieldset>
+      </div>
+    </body>
+
