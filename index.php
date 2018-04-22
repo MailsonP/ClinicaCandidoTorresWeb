9ac7f48a592b1a1acb8765b56c4ec9a66cc51cab
Hugo Silva <32854234+chernoalpha@users.noreply.github.com>
2018-04-22 03:12:29 -0300

    Atualização de Plataforma

--- index.php
+++ index.php
@@ -6,11 +6,12 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>Clinica Cândido Torres </title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
     </head>
     <body>
-        <div>yep</div>
+        <?php
+        // put your code here
+        ?>
     </body>
 </html>
