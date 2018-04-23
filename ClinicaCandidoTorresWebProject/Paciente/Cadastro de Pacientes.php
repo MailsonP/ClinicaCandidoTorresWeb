e43b16eacc6261d35ebd81ddfa49838ee068c470
FelipeSousa12 <37820585+FelipeSousa12@users.noreply.github.com>
2018-04-23 18:15:24 -0300

    Criação e Edição Classe PacienteDAO e Classe Paciente

--- ClinicaCandidoTorresWebProject/Paciente/Cadastro de Pacientes.php
+++ ClinicaCandidoTorresWebProject/Paciente/Cadastro de Pacientes.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Cadastro Pacientes</title>
+        <title>Cadastro de Pacientes</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
