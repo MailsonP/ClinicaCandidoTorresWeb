88c025bbb930a5f563e896888522ce4b4010198c
Jonathan Costa Matos <37823427+JonathanMatos97@users.noreply.github.com>
2018-04-29 16:55:44 -0300

    Criação da Tela de Login

---
+++ ClinicaCandidoTorresWebProject/TelaLogin.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Clinica Cândido Torres</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat+Alternates">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="css/cadastro.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:600" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="js/jquery-3.2.1.js"></script>
+    <script src="js/login.js"></script>
+</head>
+<body ondragstart="return false;">
+  <div class="centro">
+    <div class="conteudo">
+     <fieldset>
+            <legend>Login</legend>   
+            <form action="#" method="POST">
+            
+                <p> 
+                    <label for="loginU">Login</label> 
+                    
+                </p>
+                    <input type="text" name="login" id="loginU" required>
+                
+                <p> 
+                    <label for="senhaU">Senha</label> 
+                    
+                </p>
+                    <input type="text" name="senha" id="senhaU" required>
+                <p> 
+                              
+                <button type="submit" name="salvar">Logar</button>
+            </form>
+        </fieldset>
+    </div>
+  </div>
+<footer>
+    <h1>Copyright &copy 2018 - Fábrica de Software</h1>
+</footer>
+</body>
+</html>
