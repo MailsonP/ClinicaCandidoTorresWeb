135de42b30fbe19e26db7a0381eb1aac7b7b0019
Denilson Soares <34691643+DreshGraphics@users.noreply.github.com>
2018-04-30 01:19:11 -0300

    Index Novamente

---
+++ ClinicaCandidoTorresWebProject/Telas/Index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Clinica Cândido Torres</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat+Alternates">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway:600" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="js/jquery-3.2.1.js"></script>
+	<script src="js/login.js"></script>
+</head>
+<body ondragstart="return false;">
+	<header id="topo">
+	<input type="checkbox" id="bt_menu">
+	<label for="bt_menu">&#9776;</label>
+	<div id="right"><img src="img/cct.png"></div>
+	<nav class="menu">
+		<ul>
+			<li><a href="index.php">Inicio</a></li>
+			<li><a href="#">Cadastro</a>
+				<ul>
+					<li><a href="TelaCadastroUsuario.php">Usuário</a></li>
+					<li><a href="#">Médico</a></li>
+					<li><a href="#">Paciente</a></li>
+					<li><a href="#">Funcionário</a></li>
+					<li><a href="#">Cidade</a></li>
+					<li><a href="#">Especialização</a></li>
+				</ul>
+			</li>
+				<li>
+				<a href="#">Consulta</a>
+				<ul>
+					<li><a href="usuario/TelaUsuarioTable.php">Usuário</a></li>
+					<li><a href="#">Médico</a></li>
+					<li><a href="#">Paciente</a></li>
+					<li><a href="#">Funcionário</a></li>
+					<li><a href="#">Cidade</a></li>
+					<li><a href="#">Especialização</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Relatório</a></li>
+			<li><a href="#">Contato</a></li>
+		</ul>
+	</nav>
+	</header>
+	<div class="container-login">
+    <img src="img/logo.png">
+    <form>
+      <div class="form-input">
+        <input type="text" name="username" placeholder="Usuário">
+      </div>
+      <div class="form-input">
+        <input type="password" name="password" placeholder="Senha">
+      </div>
+        <input type="submit" name="submit" value="LOGIN" class="btn-login">
+    </form>
+     <a href="#">Esqueceu sua senha?</a>
+  </div>
+  <div class="centro">
+  	<div class="conteudo"></div>
+  </div>
+<footer>
+  	<h1>Copyright &copy 2018 - Fábrica de Software</h1>
+</footer>
+</body>
+</html>
