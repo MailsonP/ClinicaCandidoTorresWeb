0a68b50532019ebff92d846ed445559ef851fdc8
ThiagoAlencar12 <37818718+ThiagoAlencar12@users.noreply.github.com>
2018-05-01 12:27:54 -0300

    Crud Medico Sendo feito
    Construção do crud medico
    
    Co-Authored-By: mailsonp <mailsonp@users.noreply.github.com>
    Co-Authored-By: gilmarioaugusto <gilmarioaugusto@users.noreply.github.com>

---
+++ ClinicaCandidoTorresWebProject/Medico/Medico.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once '../util/baseBD.php';
+//autor - Jonathan
+class Usuario extends baseBD {
+    public function __construct($campos=array()){
+        parent::__construct();
+        $this->tabela  = "medico";
+        if(sizeof($campos)<=0){
+            $this->campos_valores = array(
+              "NOME" => NULL,
+               "TELEFONE" => NULL,
+                "EMAIL" => NULL,
+                "DTANASCIMENTO" =>NULL,
+                "CONSELHO" => NULL,
+                "ESPECIALIDADE" => NULL,
+                "FUNCAO" => NULL,
+                "TIPODEATENDIMENTO" => NULL
+            );
+        }else{
+            $this->campos_valores = $campos;
+        }
+        $this->campopk = "IDMEDICO";
+    }   
+  
+}
+
---
+++ ClinicaCandidoTorresWebProject/Telas/TelaCadastroMedico.php
@@ -0,0 +1,41 @@
+<html lang="pt-br">
+    <head>
+        <title>CADASTRO DE MEDICO</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="Cadastro" style="text-align:center">
+            
+        <fieldset>
+            <legend>Cadastro de Usuario</legend>   
+            <form action="*" method="POST">
+            
+                <p> Nome: </p>
+                <input type="text" name="nome">
+                <p> Telefone:</p>
+                <input type="text" name="telefone" >
+                <p> Email:</p>
+                <input type="text" name="email" >
+                <p> Data de Nascimento </p>
+                <input type="text" name="dtanascimento">
+                <p> Conselho: </p>
+                <input type= "text" name= "conselho">
+                <p> Especialidade: </p>
+                <input type="text" name="especialidade">
+                <p> Função:</p>
+                <input type="text" name="função">
+                <p> Tipo de Atendimento:</p>
+                <input type="text" name="tipodeatendimento">
+               
+                <br>
+                <br>
+                <button type="submit">Cadastrar</button>
+            </form>
+        </fieldset>
+      </div>
+    </body>
+</html>
+
+
+
