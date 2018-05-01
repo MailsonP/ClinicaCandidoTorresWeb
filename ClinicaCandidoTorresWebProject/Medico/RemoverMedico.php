3f969596221da6771dd1f4cdc16eb2df46d6a1db
ThiagoAlencar12 <37818718+ThiagoAlencar12@users.noreply.github.com>
2018-05-01 15:15:45 -0300

    Remover Medico
    Php Remover medico Pronto
    
    Co-Authored-By: mailsonp <mailsonp@users.noreply.github.com>
    Co-Authored-By: Jonathan Costa Matos <jonathanmatos97@users.noreply.github.com>

--- ClinicaCandidoTorresWebProject/Medico/RemoverMedico.php
+++ ClinicaCandidoTorresWebProject/Medico/RemoverMedico.php
@@ -1,2 +1,15 @@
 <?php
+require_once '../util/daoGenerico.php';
+require_once '../Medico/Medico.php';
 
+$metodo = $_GET;
+if(isset($metodo["medico"])){
+    $id = $metodo["medico"];
+   $medico = new Medico();
+   $medico->valorpk = $id;
+if($medico->deletar($medico)){
+    echo "Medico deletado com Sucesso!!";
+}else{
+    echo "Erro ao deletar Medico";
+}   
+}
