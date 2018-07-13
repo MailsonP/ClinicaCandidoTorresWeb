7b67b21132ecb8d85c2a1dadd7aed28fdb3cc4f5
ThiagoAlencar12 <37818718+ThiagoAlencar12@users.noreply.github.com>
2018-07-13 12:10:06 -0300

    Parte 1
    Parte 1, falta coisa ainda

---
+++ ClinicaCandidoTorresWebProject/Agenda/RemoverAgenda.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+require_once '../util/daoGenerico.php';
+require_once '../Agenda/Agenda.php';
+
+$metodo = $_GET;
+if(isset($metodo["agenda"])){
+    $id = $metodo["agenda"];
+    $agenda = new Agenda();
+    $agenda->valorpk = $id;
+    
+    if($agenda->deletar($agenda)){
+        echo "<script>alert('Agenda deletada com sucesso!')</script>";
+    }else{
+        
+        echo "<script>alert('Nao foi possivel deletar a agenda.')</script>";
+    }
+}
