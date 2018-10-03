cfb62cf3542d1015d2fd273f38e165f0e32230f6
FelipeSousa12 <37820585+FelipeSousa12@users.noreply.github.com>
2018-10-03 17:04:40 -0300

    Alteração Relatorios

---
+++ ClinicaCandidoTorresWebProject/Relatorio/Agenda/RelatorioAgendaPaciente.php
@@ -0,0 +1,66 @@
+<?php	
+
+	include_once("../../BancoDeDados/conexao.php");
+
+	$ID_pac = $_POST['paciente'];
+
+	$html = '<table border="1" cellspacing="0" cellpadding="10" style="border-collapse: collapse; width:100%; white-space: nowrap; z-index: 1;">';	
+	$html .= '<thead style="border-bottom: 1px solid black; font-size: 14px;">';
+	$html .= '<tr>';
+	$html .= '<th style="width: 5%; text-align: left;">ID</th>';
+	$html .= '<th style="width: 30%; text-align: left;">Paciente</th>';
+	$html .= '<th style="width: 15%; text-align: left;">Telefone</th>';
+	$html .= '<th style="width: 20%; text-align: left;">Médico</th>';
+	$html .= '<th style="width: 20%; text-align: left;">Atendimento</th>';
+	$html .= '<th style="width: 10%; text-align: left;">Data</th>';
+	$html .= '</tr>';
+	$html .= '</thead>';
+	$html .= '<tbody style="height: 5px; line-height: 5px; font-size: 12px;">';
+
+	
+	$result_transacoes = "SELECT G.IDAGENDA AS 'IDAGENDA', G.DATADEATENDIMENTO AS 'DATADEATENDIMENTO', P.NOME AS 'NOMEDOPACIENTE' ,M.NOME AS 'NOMEDOMEDICO',A.TIPOATENDIMENTO AS 'TIPODEATENDIMENTO', P.CELULAR AS 'CELULAR' FROM AGENDA AS G INNER JOIN PACIENTE AS P ON IDPACIENTE = ID_PACIENTE INNER JOIN MEDICO AS M ON IDMEDICO = ID_MEDICO INNER JOIN ATENDIMENTO AS A ON IDATENDIMENTO = ID_ATENDIMENTO WHERE ID_PACIENTE = $ID_pac";
+	$resultado_trasacoes = mysqli_query($conn, $result_transacoes);
+	while($row_transacoes = mysqli_fetch_assoc($resultado_trasacoes)){
+		$html .= '<tr style="border-bottom: 1px solid black"><td style="font-weight: 600; width: 5%;">'.$row_transacoes['IDAGENDA'] . "</td>";
+		$html .= '<td style="width: 30%;">'.$row_transacoes['NOMEDOPACIENTE'] . "</td>";
+		$html .= '<td style="width: 15%;">'.$row_transacoes['CELULAR'] . "</td>";
+		$html .= '<td style="width: 20%;">'.$row_transacoes['NOMEDOMEDICO'] . "</td>";
+		$html .= '<td style="width: 20%;">'.$row_transacoes['TIPODEATENDIMENTO'] . "</td>";
+		$html .= '<td style="width: 10%;">'.date("d/m/Y", strtotime($row_transacoes['DATADEATENDIMENTO'])) . "</td></tr>";		
+	}
+	
+	$html .= '</tbody>';
+	$html .= '</table>';
+	$html .= '<div style="position: absolute; bottom: 0;"><h6 style="font-family: sans-serif; text-align: center; padding-bottom: -20px;">Campos Monteiro 1141 Centro, Av. Josefa Nogueira Monteiro,</h6>';
+	$html .= '<h6 style="font-family: sans-serif; text-align: center; padding-bottom: -20px;">Icó - CE, 63430-000</h6>';
+	$html .= '<h6 style="font-family: sans-serif; text-align: center;">(88) 99915-1098</h6></div>';
+
+	
+	//referenciar o DomPDF com namespace
+	use Dompdf\Dompdf;
+
+	// include autoloader
+	require_once("../dompdf/autoload.inc.php");
+
+	//Criando a Instancia
+	$dompdf = new DOMPDF();
+	
+	// Carrega seu HTML
+	$dompdf->load_html('
+			<div align="center"><img src="../../img/logo.png"></div>
+			<h3 style="text-align: center; font-family: sans-serif; padding: -15px; margin: 0;">Clinica Cândido Torres</h3>
+			<h4 style="text-align: center; font-family: sans-serif;">Relatório de Agenda</h4>
+			'. $html .'
+		');
+
+	//Renderizar o html
+	$dompdf->render();
+
+	//Exibibir a página
+	$dompdf->stream(
+		"Relatorio_CandidoTorres.pdf", 
+		array(
+			"Attachment" => false //Para realizar o download somente alterar para true
+		)
+	);
+?>
--- ClinicaCandidoTorresWebProject/Telas/TelaRelatorio.php
+++ ClinicaCandidoTorresWebProject/Telas/TelaRelatorio.php
@@ -2,6 +2,8 @@
 session_start();
 include_once '../Login/ProtectPaginas.php';
 include_once '../Medico/Medico.php';
+include_once '../Paciente/Paciente.php';
+include_once '../Atendimento/Atendimento.php';
 protect();
 
 if(isset($_SESSION["tipoUsuario"])){
@@ -10,7 +12,12 @@ if(isset($_SESSION["tipoUsuario"])){
    
    //PARA LISTAR NO COMBOBOX DO MODAL
    $medic = new Medico();
+   $pac = new Paciente();
+   $tipoAten = new Atendimento();
    $medic->retornaTudo($medic);
+   $pac->retornaTudo($pac);
+   $tipoAten->retornaTudo($tipoAten);
+
 
 ?>
 <!DOCTYPE html>
@@ -37,13 +44,13 @@ if(isset($_SESSION["tipoUsuario"])){
 	<div class="container">
 		<div class="row" style="text-align: center; cursor: pointer;">
 			<div class="col-sm-3" style="background: cyan; height: 50px; line-height: 50px; font-weight: 600;">
-				<a data-toggle="modal" data-target="#exampleModal">Atendimento por Profissional</a>
+				<a data-toggle="modal" data-target="#ModalMedico">PROFISSIONAL</a>
 			</div>
 			<div class="col-sm-3" style="background: #f23355; height: 50px; line-height: 50px; font-weight: 600;">
-				Paciente
+				<a data-toggle="modal" data-target="#ModalPaciente">PACIENTE</a>
 			</div>
 			<div class="col-sm-3" style="background: cyan; height: 50px; line-height: 50px; font-weight: 600;">
-				Profissional
+				<a data-toggle="modal" data-target="#ModalAtendimento">ATENDIMENTO</a>
 			</div>
 			<div class="col-sm-3" style="background: #f23355; height: 50px; line-height: 50px; font-weight: 600;">
 				d
@@ -52,7 +59,7 @@ if(isset($_SESSION["tipoUsuario"])){
 	</div>
 
 	<!-- MODAL DE ESCOLHA DE MEDICO -->
-                 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                 <div class="modal fade" id="ModalMedico" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                       <div class="modal-dialog" role="document">
                         <div class="modal-content">
                           <div class="modal-header">
@@ -61,7 +68,7 @@ if(isset($_SESSION["tipoUsuario"])){
                               <span aria-hidden="true">&times;</span>
                             </button>
                           </div>
-                          <form action="../Relatorio/Agenda/RelatorioAgendaProfissional.php" method="POST">
+                          <form action="../Relatorio/Agenda/RelatorioAgendaProfissional.php" method="POST" target="_blank">
                           <div class="modal-body">
                             <div class="conteudo">              
               					     	<select class="custom-select" style="text-transform: uppercase;" name="medico">
@@ -79,6 +86,64 @@ if(isset($_SESSION["tipoUsuario"])){
                       </div>
                   </div>
  <!-- F I M  M O D A L -->
+
+ <!-- MODAL DE ESCOLHA DE PACIENTE -->
+                 <div class="modal fade" id="ModalPaciente" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                      <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">SELECIONE UM PACIENTE</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <form action="../Relatorio/Agenda/RelatorioAgendaPaciente.php" method="POST" target="_blank">
+                          <div class="modal-body">
+                            <div class="conteudo">              
+                              <select class="custom-select" style="text-transform: uppercase;" name="paciente">
+                                <?php while ($dadoPac = $pac->retornaDados("object")) { ?>  
+                                <option value="<?php echo $dadoPac->IDPACIENTE; ?>"><?php echo $dadoPac->NOME; ?></option>
+                                <?php  }  ?>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="modal-footer">
+                            <button type="submit" class="btn btn-success" style="margin: 0 auto;">GERAR RELATÓRIO</button>
+                          </div>
+                        </form>
+                        </div>
+                      </div>
+                  </div>
+ <!-- F I M  M O D A L -->
+ <!-- MODAL DE ESCOLHA DE ATENDIMENTO -->
+                 <div class="modal fade" id="ModalAtendimento" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                      <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">SELECIONE UM PACIENTE</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <form action="../Relatorio/Agenda/RelatorioAgendaPaciente.php" method="POST" target="_blank">
+                          <div class="modal-body">
+                            <div class="conteudo">              
+                              <select class="custom-select" style="text-transform: uppercase;" name="paciente">
+                                <?php while ($dadoAtend = $tipoAten->retornaDados("object")) { ?>  
+                                <option value="<?php echo $dadoAtend->IDATENDIMENTO; ?>"><?php echo $dadoAtend->TIPOATENDIMENTO; ?></option>
+                                <?php  }  ?>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="modal-footer">
+                            <button type="submit" class="btn btn-success" style="margin: 0 auto;">GERAR RELATÓRIO</button>
+                          </div>
+                        </form>
+                        </div>
+                      </div>
+                  </div>
+ <!-- F I M  M O D A L -->
+
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
