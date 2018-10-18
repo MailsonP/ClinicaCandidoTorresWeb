52be4ec678387fb042f9871ec951d5232a19d2e4
Denilson Soares <34691643+DreshGraphics@users.noreply.github.com>
2018-10-18 03:19:36 -0300

    Alterações

--- ClinicaCandidoTorresWebProject/Telas/TelaRelatorio.php
+++ ClinicaCandidoTorresWebProject/Telas/TelaRelatorio.php
@@ -26,7 +26,10 @@ if(isset($_SESSION["tipoUsuario"])){
 <head>
 	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
-	 <title>Relatório</title>
+	  <title>Relatório</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat+Alternates">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:600" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
   	<style type="text/css">
@@ -34,52 +37,55 @@ if(isset($_SESSION["tipoUsuario"])){
 			box-sizing: border-box;
 		}
 
-  		@media screen and (max-width: 575px){
-  			div .row{
-  				margin: 0 20px;
-  			}
-  		}
+    h2{
+      text-align: center;
+      color: #f3f3f3;
+      font-family: "raleway", sans-serif;
+      margin: 20px 0;
+    }
+
+    .cor-row{
+      margin-top: 50px;
+      padding: 10px;
+      background: #202020;
+      font-family: "raleway", sans-serif;
+      -webkit-box-shadow: 0px 0px 5px 1px rgba(0,0,0,0.18);
+      -moz-box-shadow: 0px 0px 5px 1px rgba(0,0,0,0.18);
+      box-shadow: 0px 0px 5px 1px rgba(0,0,0,0.18);
+    }
+
+  	}
   	</style>	
 </head>
 <body>
+  <?php include '../util/nav.php' ?>
+
 	<div class="container">
-		<div class="row" style="text-align: center; cursor: pointer;">
-			<div class="col-sm-3" style="background: cyan; height: 50px; line-height: 50px; font-weight: 600;">
-				<a data-toggle="modal" data-target="#ModalMedico">AGENDA POR PROFISSIONAL</a>
-			</div>
-			<div class="col-sm-3" style="background: #f23355; height: 50px; line-height: 50px; font-weight: 600;">
-				<a data-toggle="modal" data-target="#ModalPaciente">AGENDA POR PACIENTE</a>
-			</div>
-			<div class="col-sm-3" style="background: cyan; height: 50px; line-height: 50px; font-weight: 600;">
-				<a data-toggle="modal" data-target="#ModalAtendimento">AGENDA POR ATENDIMENTO</a>
-			</div>
-			<div class="col-sm-3" style="background: #f23355; height: 50px; line-height: 50px; font-weight: 600;">
-        <a data-toggle="modal" data-target="#ModalAgendaData">AGENDA POR DATA</a>
-			</div>
-		</div>
-    <div class="row" style="text-align: center; cursor: pointer;">
-      <div class="col-sm-2" style="background: #f23355; height: 50px; line-height: 50px; font-weight: 600;">
-        <a data-toggle="modal" data-target="#ModalSexo">PACIENTE POR SEXO</a>
+		<div class="row cor-row">
+      <div class="form-group col-sm-6 c1">
+        <h2>Agenda</h2>
+				<a class="btn btn-light btn-lg btn-block bt" data-toggle="modal" data-target="#ModalMedico">POR PROFISSIONAL</a>
+				<a class="btn btn-light btn-lg btn-block bt" data-toggle="modal" data-target="#ModalPaciente">POR PACIENTE</a>
+				<a class="btn btn-light btn-lg btn-block bt" data-toggle="modal" data-target="#ModalAtendimento">POR ATENDIMENTO</a>
+        <a class="btn btn-light btn-lg btn-block bt" data-toggle="modal" data-target="#ModalAgendaData">POR DATA</a>
       </div>
-      <div class="col-sm-2" style="background: cyan; height: 50px; line-height: 50px; font-weight: 600;">
-        <a data-toggle="modal" data-target="#ModalPacienteDia">PACIENTE POR DIA</a>
+
+      <div class="form-group col-sm-6 c2">
+        <h2>Paciente</h2>
+        <a class="btn btn-light btn-lg btn-block bt" data-toggle="modal" data-target="#ModalSexo">POR SEXO</a>
+        <a class="btn btn-light btn-lg btn-block bt" data-toggle="modal" data-target="#ModalPacienteDia">POR DIA</a>
+        <a class="btn btn-light btn-lg btn-block bt" data-toggle="modal" data-target="#ModalPacienteMes">POR MES</a>
+        <a class="btn btn-light btn-lg btn-block bt" data-toggle="modal" data-target="#ModalPacienteAno">POR ANO</a>
+        <a class="btn btn-light btn-lg btn-block bt" data-toggle="modal" data-target="#ModalPacienteMesNiver">MES NASCIMENTO</a>
+        <a class="btn btn-light btn-lg btn-block bt" data-toggle="modal" data-target="#ModalPacienteAnoNiver">ANO NASCIMENTO</a>
       </div>
-      <div class="col-sm-2" style="background: #f23355; height: 50px; line-height: 50px; font-weight: 600;">
-        <a data-toggle="modal" data-target="#ModalPacienteMes">PACIENTE POR MES</a>
-      </div>
-      <div class="col-sm-2" style="background: cyan; height: 50px; line-height: 50px; font-weight: 600;">
-        <a data-toggle="modal" data-target="#ModalPacienteAno">PACIENTE POR ANO</a>
-      </div>
-      <div class="col-sm-2" style="background: #f23355; height: 50px; line-height: 50px; font-weight: 600;">
-        <a data-toggle="modal" data-target="#ModalPacienteMesNiver">MES NASCIMENTO</a>
-      </div>
-      <div class="col-sm-2" style="background: cyan; height: 50px; line-height: 50px; font-weight: 600;">
-        <a data-toggle="modal" data-target="#ModalPacienteAnoNiver">ANO NASCIMENTO</a>
-      </div>
+
     </div>
 	</div>
 
-<?php include '../util/modalRelatorio.php' ?>
+  <?php include '../util/modalRelatorio.php' ?>
+
+  <?php include '../util/footer.php' ?>
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
